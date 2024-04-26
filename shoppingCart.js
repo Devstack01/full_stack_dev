79ef27261596b470f9742511ba773e059ee2f945
Devstack01 <164265086+Devstack01@users.noreply.github.com>
2024-04-25 21:42:40 -0400

    Advanced JavaScript Task

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,59 +1,64 @@
-//step 1
+class Item {
+    constructor(name, price, discount = 0) {
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+    }
+}
 let shoppingCart = {
-    items:[],
-    addItem: function(item){
-        this.items.push(item);
+    items: [],
+
+    addItem: (item)  => {
+        //console.debug('addItem')
+        shoppingCart.items.push(item);
+    },
+    removeItem: (index) => {
+        //console.debug('removeItem')
+        shoppingCart.items.splice(index, 1);
+    },
+    calculateTotal: () => {
+        debugger;
+
+        //console.debug('calculateTotal')
+        let total = 0;
+        for (let i = 0; i < shoppingCart.items.length; i++) {
+            //console.debug(i)
+            total += shoppingCart.items[i].price;
+        }
+        return total;
+    },
+    applyDiscountCode: (code) => {
+        switch (code) {
+            case 'SAVE10':
+                return shoppingCart.calculateTotal() * 0.9; // Apply 10% discount
+            case 'SAVE20':
+                return shoppingCart.calculateTotal() * 0.8; // Apply 20% discount
+            default:
+                return shoppingCart.calculateTotal();
+        }
+    },
+    getCurrentItems: () => {
+        return shoppingCart.items.map(item => item.name) ;
+    },
+    getItemByName: () => {
+        return shoppingCart.items.find(item => item.name);
+        
     }
 };
-
-//step 2
-function Item(name, price){
-    this.name = name;
-    this.price = price;
-}
-
-//step 3
 let item1 = new Item('Shirt', 20);
 let item2 = new Item('Pants', 30);
-
+let item3 = new Item('temp', 100);
 shoppingCart.addItem(item1);
 shoppingCart.addItem(item2);
 
-//step 4
-shoppingCart.removeItem = function(index){
-    this.items.splice(index, 1);
-};
+shoppingCart.addItem({ name: 'temp', price: 40 });
 
-//step 5
-shoppingCart.calculateTotal = function() {
-    let total = 0;
-    for (let i = 0; i < this.items.length; i++) {
-        total += this.items[i].price; 
-    }
-    return total;
-};
-
-//step 6
-shoppingCart.applyDiscountCode = function(code){
-    switch (code) {
-        case 'SAVE10':
-            return this.calculateTotal() * 0.9;
-            case 'SAVE20':
-                return this.calculateTotal() * 0.8;
-                default:
-                    return this.calculateTotal();
-    }
-};
-
-//step 7
-shoppingCart.getCurrentItems = function(){
-    return this.items.map(item => item.name);
-};
-
-//step 8
-
-console.log(shoppingCart.getCurrentItems());
+console.log(`${shoppingCart.getCurrentItems()}`); // Output: ['Shirt', 'Pants']
 shoppingCart.removeItem(0);
-console.log(shoppingCart.getCurrentItems());
-console.log(shoppingCart.calculateTotal());
-console.log(shoppingCart.applyDiscountCode('SAVE10'));
+console.log(`${shoppingCart.getCurrentItems()}`); // Output: ['Pants']
+
+console.log(`${shoppingCart.calculateTotal()}`); // Output: 30
+
+console.log(`${shoppingCart.applyDiscountCode('SAVE10')}`); // Output: 27
+
+console.log(shoppingCart.getItemByName('Pants')); // Output: { name: 'Pants', price: 30, discount: 0}
